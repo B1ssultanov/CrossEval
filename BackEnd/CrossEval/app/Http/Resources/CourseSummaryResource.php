98488e781411d2f7c3ddbf00cd75dad7669144b9
B1ssultanov <95929680+B1ssultanov@users.noreply.github.com>
2024-08-14 17:46:04 +0500

    CROSSEVAL main page API update
    Updated main page API so now it returns teacher name in it.

--- BackEnd/CrossEval/app/Http/Resources/CourseSummaryResource.php
+++ BackEnd/CrossEval/app/Http/Resources/CourseSummaryResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class CourseSummaryResource extends JsonResource
 {
@@ -15,15 +16,24 @@ class CourseSummaryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $teacher = DB::table('courses')
+            ->join('user_courses', 'user_courses.course_id', '=', 'courses.id')
+            ->join('users', 'users.id', '=', 'user_courses.user_id')
+            ->where('courses.id', $this->id)
+            ->where('users.role', 'supervisor')
+            ->select('users.*')
+            ->first();
+
+        $teacherName = $teacher ? $teacher->name . ' ' . $teacher->surname : 'No teacher assigned';
+
         $data = [
             'id'           => $this->id,
             'name'         => $this->name,
             'code'         => $this->code,
             'invite_code'  => $this->invite_code,
             'course_group' => $this->course_group,
+            'teacher_name' => $teacherName,
         ];
-
-        Cache::put('courseSummary' . $this->id, $data);
 
         return $data;
     }
