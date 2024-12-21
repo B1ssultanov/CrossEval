3418ccc013a8ba36eb1c95d76828115f70de5c04
B1ssultanov <95929680+B1ssultanov@users.noreply.github.com>
2024-12-21 13:08:28 +0500

    CROSSEVAL-65 add user info API
    Added API with the get route "/user" which will give the full information about the user

--- BackEnd/CrossEval/routes/api.php
+++ BackEnd/CrossEval/routes/api.php
@@ -55,6 +55,7 @@ Route::prefix('v1')->namespace('App\Http\Controllers\Api')->group(function () {
 
         Route::get('/user/student/grades',    'UserController@student_grades');
         Route::get('/user/supervisor/grades', 'UserController@supervisor_grades');
+        Route::get('/user',                   'UserController@info');
         Route::post('/user/profile',          'UserController@profile_update');
         Route::delete('/user',                'UserController@delete');
         Route::delete('/user/course',         'UserController@remove_from_course');
