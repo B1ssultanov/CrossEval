efbcaf827ca68e96a2effb63bb4a9b8c44a1958a
root <yedyge2004@mail.ru>
2025-05-03 12:42:21 +0000

    server commit

--- BackEnd/CrossEval/config/cors.php
+++ BackEnd/CrossEval/config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'X-Auth-Token', 'Authorization', 'Origin'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
